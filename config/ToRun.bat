f7ec0a79bf8d3edd5acc6e97bec019d5d75ac545
lastagile <lastdanc@gmail.com>
2013-12-22 19:17:39 +0800

    solve the problem of start with windows
    changed the current working dir when start with windows.

--- config/ToRun.bat
+++ config/ToRun.bat
@@ -1,4 +1,4 @@
 echo %date% >>runTime.txt
 echo %time% >>runTime.txt
 
-jhProtominer -o 112.124.13.238:28988 -u PYAYBuu8NQCq4P15Vg2gPjBUwebz1NCY41 -p x -t 2 1>out.txt 
+jhProtominer -o 112.124.13.238:28988 -u PYAYBuu8NQCq4P15Vg2gPjBUwebz1NCY41 -p x -t 2
--- run.cpp
+++ run.cpp
@@ -11,10 +11,11 @@
 
 using namespace std;
 
-#include "pdh.h"  
-#pragma comment(lib, "Pdh.lib") //显式连接Pdh.lib  
+// used for get cpu counting
+#include "pdh.h"
+#pragma comment(lib, "Pdh.lib") //Pdh.lib  
 
-bool bBack=true;
+bool bBack=false;
 int WAITE_TIME=600000;
 int CHECK_TIME=3000;
 int CPUUSEAGE=20;
@@ -32,17 +33,16 @@ bool inactive()
 		return false;
 }
 
+// run then mine program
 bool creatProcess(PROCESS_INFORMATION *pi)
 {
 	char chPath[301];
-
-	::GetCurrentDirectory(300,(LPTSTR)chPath);//得到当前目录
-
-	char path[200]= "//ToRun.cmd";
-
-	strcat(chPath,path);
 	STARTUPINFO si;
-	
+	char path[200]= "\\ToRun.bat";
+
+	::GetCurrentDirectory(300,(LPTSTR)chPath);
+	strcat(chPath,path);
+
 	ZeroMemory( pi, sizeof(*pi) );
 	ZeroMemory( &si, sizeof(si) );
 
@@ -110,7 +110,7 @@ void runBack()
 	HWND hwnd;
 	if(hwnd=::FindWindow("ConsoleWindowClass",NULL)) //
 	{
-		::ShowWindow(hwnd,SW_HIDE); //后台运行
+		::ShowWindow(hwnd,SW_HIDE); //run background
 	}
 }
 
@@ -127,64 +127,102 @@ void readConfig()
 		inf >> CPUUSEAGE;
 	}
    
-
 	inf.close();
+}
+
+// set current dir. Very useful when this program is star with the system.
+void setCurrentDir()
+{
+	char strPath[512];
+	string a;
+	int j;
+
+	GetModuleFileName(NULL,strPath, 512); // get current file path
+
+	for(j=strlen(strPath);strPath[j]!='\\';j--);
+		strPath[j]='\0';
+
+	SetCurrentDirectory(strPath);  // set current working dir
+	GetCurrentDirectory(512,strPath);
+
+	return;
+}
+
+HQUERY hQuery = NULL;                       //计数器对象句柄  
+HCOUNTER hCounter = NULL;                   //计数器句柄  
+PDH_FMT_COUNTERVALUE DisplayValue;          //计数器值  
+DWORD dwCounterType = 0;                    //计数器类型  
+
+PDH_STATUS initCpuData()
+{
+	PDH_STATUS status;
+	
+	status = PdhOpenQuery(NULL, 0, &hQuery);    //打开计数器对象
+
+	if (ERROR_SUCCESS == status)  
+	{  
+		//PdhAddCounter()函数添加计数器，参数中Processor代表计数器对象，(0)表示第1个处理器计数实例。
+		//如果计算机上有两个处理器，则第2个处理器计数实例用(1)表示  
+		//% Processor Time表示计数器（取得处理器占用时间）,注意%后面有个空格  
+		status = PdhAddCounter(hQuery, "\\Processor(0)\\% Processor Time", 0, &hCounter);
+
+		if (ERROR_SUCCESS == status)  
+		{  
+			// PdhCollectQueryData()函数收集数据需两次调用, PdhCollectQueryDataEx只需一次
+			PdhCollectQueryData(hQuery);        //首次收集数据  
+		}
+	}
+	return status;
+}
+
+bool getCpuData()
+{
+	PDH_STATUS status = -1;  
+	PdhCollectQueryData(hQuery);    //再次收集数据
+	status = PdhGetFormattedCounterValue(hCounter,
+				PDH_FMT_DOUBLE,   
+				&dwCounterType, 
+				&DisplayValue);
+
+	return DisplayValue.doubleValue < CPUUSEAGE;
 }
 
 int main(int argc, char* argv[])
 {
 	bool bCreated = false;
 	bool bInactive = false;
+	bool bUsageLow = true;
+	PDH_STATUS cpuStatus = -1;
+
 	PROCESS_INFORMATION pi;
-	
+	setCurrentDir();
 	
 	readConfig();
+
 	if(bBack)
 	{
 		runBack();
 	}
 
-	PDH_STATUS status = -1;  
-	HQUERY hQuery = NULL;                       //计数器对象句柄  
-	HCOUNTER hCounter = NULL;                   //计数器句柄  
-	PDH_FMT_COUNTERVALUE DisplayValue;          //计数器值  
-	DWORD dwCounterType = 0;                    //计数器类型  
-	status = PdhOpenQuery(NULL, 0, &hQuery);    //打开计数器对象
-
-	if (ERROR_SUCCESS == status)  
-	{  
-	  //PdhAddCounter()函数添加计数器，参数中Processor代表计数器对象，(0)表示第1个处理器计数实例。
-	  //如果计算机上有两个处理器，则第2个处理器计数实例用(1)表示  
-	  //% Processor Time表示计数器（取得处理器占用时间）,注意%后面有个空格  
-	  status = PdhAddCounter(hQuery, "\\Processor(0)\\% Processor Time", 0, &hCounter);
-
-	  if (ERROR_SUCCESS == status)  
-	  {  
-		  // PdhCollectQueryData()函数收集数据需两次调用, PdhCollectQueryDataEx只需一次
-		  printf("CPU占用 首次收集数据  \n");  
-		  PdhCollectQueryData(hQuery);        //首次收集数据  
-	  }
+	if(CPUUSEAGE < 100)
+	{
+		cpuStatus = initCpuData();
 	}
-
 
     while(1)
     {
         Sleep(CHECK_TIME);
 
-		PdhCollectQueryData(hQuery);    //再次收集数据
-
-		status = PdhGetFormattedCounterValue(hCounter,
-					PDH_FMT_DOUBLE,   
-					&dwCounterType, 
-					&DisplayValue);
-
-		//取得收集的数据值
-		//printf("CPU占用 %.2f\n", DisplayValue.doubleValue);  
+		//get CPU usage
+		if(ERROR_SUCCESS == cpuStatus)
+		{
+			bUsageLow = getCpuData();
+		}
 
 		bInactive = inactive();
 		if(false == bCreated)
 		{
-			if(bInactive && DisplayValue.doubleValue < CPUUSEAGE)
+			if(bInactive && bUsageLow)
 			{
 				bCreated = creatProcess(&pi);
 			}
@@ -206,6 +244,6 @@ int main(int argc, char* argv[])
 
     }
 	
-	PdhCloseQuery(hQuery);                  //关闭计数器对象  
+	PdhCloseQuery(hQuery);                  //close counting
 	return 0;
 }
